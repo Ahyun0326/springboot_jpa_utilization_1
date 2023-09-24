2a8ae9e8d5d065640c5facc5502a9dc4442476b6
Yeseo <chinkl0110@naver.com>
2023-09-24 19:41:12 +0900

    상품 등록 폼

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id;
+
+    @NotEmpty(message = "아이템 이름을 입력하세요.")
+    private String name;
+
+    private int price;
+    private int stockQuantity;
+
+    private String author;
+    private String isbn;
+}
